00322d6e36fc22d598697a757c770ec3270e952f
Djent- <inflames2361@aim.com>
2014-05-23 13:18:56 -0400

    Added "greek names.lua"

---
+++ greek names.lua
@@ -0,0 +1,45 @@
+greekGods = {"Aphrodite", "Apollo", "Ares", "Artemis", "Athena", "Demeter", "Dionysus", "Hades",
+"Hephaestus", "Hera", "Hermes", "Hestia", "Poseidon", "Zeus"}
+greekPrimordialDeities = {"Aether", "Ananke", "Chaos", "Chronos", "Erebos", "Eros", "Gaia", "Gaea",
+"Ge", "Hemera", "Hypnos", "Nesoi", "Nyx", "Night", "Uranus", "Ourea", "Phanes", "Pontus", "Tartarus",
+"Thalassa", "Thanatos"}
+greekTwelveTitans = {"Hyperion", "Iapetus", "Coeus", "Crius", "Kronos", "Mnemosyne", "Oceanus",
+"Phoebe", "Rhea", "Tethys", "Theia", "Themis"}
+greekOtherTitans = {"Asteria", "Astraeus", "Atlas", "Aura", "Dione", "Eos", "Epimetheus", "Eurybia",
+"Eurynome", "Helios", "Clymene", "Asia", "Lelantos", "Leto", "Menoetius", "Metis", "Ophion", "Pallas",
+"Perses", "Prometheus", "Selene", "Styx"}
+greekGigantes = {"The Hekatonkheires", "Centimanes", "the Hundred-Handed Ones", "Briareus", "Aigaion",
+"Cottus", "Gyges", "Agrius", "Alcyoneus", "Aloadae", "Otos", "Otis", "Ephialtes". "Antaeus",
+"Argus Panoptes", "Cyclopes", "Arges", "Brontes", "Steropes", "Polyphemus", "Enceladus", 
+"The Gegenees", "Geryon", "The Laestrygonians", "Orion", "Porphyrion", "Talos", "Tityos", "Typhon"}
+greekPersonifiedConcepts = {"Achlys", "Adephagia", "Adikia", "Aergia", "Agon", "Aidos", "Aisa",
+"Alala", "Alastor", "Aletheia", "The Algea", "Achos", "Ania", "Lupe", "Alke", "Amechania",
+"The Amphilogiai", "Anaideia", "The Anreoktasiai", "Angelia", "Apate", "Apheleia", "Aporia",
+"The Arae", "Arete", "Ate", "Bia", "Caerus", "Corus", "Deimos", "Dikaiosyne", "Dike", "Dolos",
+"Dysnomia", "Dyssebeia", "Eirene", "Ekecheiria", "Eleos", "Elpis", "Epiphron", "Eris", "The Erotes",
+"Anteros", "Eros", "Hedylogos", "Himeros", "Pothos", "Eucleia", "Eulabeia", "Eunomia", "Eupheme",
+"Eupraxia", "Eusebeia", "Euthenia", "Gelos", "Geras", "Harmonia", "Hebe", "Hedone", "Heimarmene",
+"Homados", "Homonoia", "Horkos", "Horme", "Hybris", "The Hysminai", "Ioke", "Kakia", "Kalokagathia",
+"The Keres", "Koalemos", "Kratos", "Kydoimos", "Lethe", "Limos", "The Litae", "Lyssa", "The Machai",
+"Mania", "The Moirai", "Clotho", "Lachesis", "Atropos", "Momus", "Moros", "The Neikia", "Nemesis",
+"Nike", "Nomos", "Oizys", "The Oneiroi", "Epiales", "Morpheus", "Phantasos", "Phobetor", "Palioxis",
+"Peitharchia", "Peitho", "Penia", "Penthus", "Pepromene", "Pheme", "Philophrosyne", "Philotes",
+"Phobos", "The Phonoi", "Phrike", "Phthonus", "Pistis", "Poine", "Polemos", "Ponos", "Poros",
+"Praxidike", "Proioxis", "Prophasis", "The Pseudologoi", "Ptocheia", "Soter", "Soteria", "Sophrosyne",
+"Techne", "Thanatos", "Thrasos", "Tyche", "Zelos"}
+greekChthonicDeities = {"Amphiaraus", "Angelos", "Askalaphos", "Cerberus", "Charon", "Empusa",
+"Erebos", "The Erinyes", "Alecto", "Tisiphone", "Megaera", "Hecate", "Aiakos", "Minos", "Rhadamanthys",
+"Keuthonymos", "Cronus", "Lamia", "Lampades", "Gorgyra", "Orphne", "Macaria", "Melinoe", "Menoetes",
+"Mormo", "Nyx", "Persephone", "Acheron", "Kokytos", "Lethe", "Phlegethon", "Styx", "Tartarus",
+"Thanatos"}
+greekSeaDeities = {"Aegaeon", "Achelous", "Amphitrite", "Benthesikyme", "Brizo", "Ceto", "Charybdis",
+"Cymopoleia", "Delphin", "Eidothea", "Glaucus", "Gorgons", "Stheno", "Euryale", "Medusa",
+"The Graeae", "Deino", "Enyo", "Pemphredo", "The Harpies", "Aello", "Aellope", "Aellopous",
+"Ocypete", "Podarge", "Podarke", "Celaeno", "Nicothoe", "Hippocampi", "Hydros", "The Ichthyocentaurs",
+"Bythos", "Aphros", "Karkinos", "Ladon", "Leucothea", "Nereides", "Thetis", "Arethusa", "Galene",
+"Psamathe", "Nereus", "Nerites", "Palaemon", "Phorcys", "Proteus", "Scylla", "The Sirens", "Aglaope",
+"Aglaophonos", "Aglaopheme", "Himerope", "Leucosia", "Ligeia", "Molpe", "Parthenope", "Peisinoe",
+"Raidne", "Teles", "Thelchtereia", "Thelxiope", "Thelxiepeia", "The Telchines", "Actaeus",
+"Argyron", "Atabyrius", "Chalcon", "Chryson", "Damon", "Demonax", "Damnameneus", "Dexithea",
+"Lycos", "Lyktos", "Lysagora", "Makelo", "Megalesius", "Mylas", "Nikon", "Ormenos", "Simon", "Skelmis",
+"Tethys", "Thalassa", "Thaumas" "Thoosa", "Triteia", "Triton", "Tritones"}
